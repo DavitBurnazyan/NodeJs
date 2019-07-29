c85238f5e852c379232709cb27c4f66afe6860ec
Davit <burnazyandavit1996@gmail.com>
2019-07-29 15:00:01 +0400

    update

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 const express = require('express');
-const config = require('./config');
+const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const routes = require('./routes/index');
 const mongoose = require('mongoose');
 const checkModel = require('./helpers/checkModel');
-
+const env = dotenv.config();
 const app = express();
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-mongoose.connect(config.Db.url, {
+mongoose.connect(env.parsed.DB_URL, {
   useNewUrlParser: true,
 }).then(() => {
 	console.log('Successfully connected to the database');
-	app.listen(config.PORT, () => console.log(`PORT is ${config.PORT}`));
+	app.listen(env.parsed.PORT, () => console.log(`PORT is ${env.parsed.PORT}`));
 }).catch(() => {
 	console.log('Could not connect to the database. Exiting now...');
 	process.exit();
 });
 
 routes(app);
+
+
+
 
--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  PORT: process.env.PORT || 3000,
-  Db: {
-    url: 'mongodb://localhost:3000/db',
-  },
-};
